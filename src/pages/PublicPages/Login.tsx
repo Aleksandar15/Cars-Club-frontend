535372c441d2de1f4d8f9c3dd06426eee3f78718
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-03-14 17:14:40 +0100

    Improved UI login component

--- src/pages/PublicPages/Login.tsx
+++ src/pages/PublicPages/Login.tsx
@@ -1,30 +1,22 @@
+import { FormEvent, MouseEvent } from "react";
 import { Button, Form } from "react-bootstrap";
 import useMyNavigate from "../../hooks/useMyNavigate/useMyNavigate";
 
 const Login = () => {
   const navigatePage = useMyNavigate();
 
+  const loginSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1 className="text-info text-center mb-5">LOGIN</h1>
       <div className="d-flex flex-column align-items-center ">
         <Form
-          //  onSubmit={}
-          className="pb-4 mb-4 d-flex flex-column w-25 align-items-center "
+          onSubmit={loginSubmit}
+          className="pb-4 mb-4 d-flex flex-column w-25 align-items-center form-login "
         >
-          <Form.Label id="username" className="text-primary mb-0 ">
-            Username:
-          </Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Username"
-            name="username"
-            id="username"
-            aria-describedby="username"
-            // onChange={}
-            // value={}
-            className="mb-2"
-          />
           <Form.Label id="email" className="text-primary mb-0 ">
             E-mail:
           </Form.Label>
@@ -41,33 +33,35 @@ const Login = () => {
           <Form.Label id="password" className="text-primary mb-0 ">
             Password:
           </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            id="password"
-            aria-describedby="password"
-            // onChange={}
-            // value={}
-            className="mb-4"
-          />
+          <div className="input-group">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="password"
+              aria-describedby="password"
+              // onChange={}
+              // value={}
+              className="mb-4"
+            />
+          </div>
           <Button
             variant=" btn bg-light btn-outline-info text-info  "
             type="submit"
           >
             LOGIN
           </Button>
+          <div className="mt-5 ms-1 mb- d-flex  align-items-center justify-content-space-between">
+            <p className="text-danger pt-2 me-3">Already have an account?</p>
+            <Button
+              variant="btn bg-light btn-outline-success pb-1 mb-1  text-success btn-sm "
+              type="button"
+              onClick={() => navigatePage("/register")}
+            >
+              REGISTER
+            </Button>
+          </div>
         </Form>
-        <div className="mt-1 ms-1 mb-2 d-flex  align-items-center justify-content-space-between">
-          <p className="text-danger pt-2 me-3">Already have an account?</p>
-          <Button
-            variant="btn bg-light btn-outline-success pb-1 mb-1  text-success btn-sm "
-            type="button"
-            onClick={() => navigatePage("/register")}
-          >
-            REGISTER
-          </Button>
-        </div>
       </div>
     </>
   );

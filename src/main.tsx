3fd80c807c5a495632ec589ace5a2a149e7a6bad
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-03-15 14:59:58 +0100

    REDUX setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import App from "./App";
 // import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+// Deprecated createStore
+import rootReducer from "./redux/reducer/rootReducer";
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
---
+++ src/redux/reducer/rootReducer.tsx
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+// import verifyReducer from "./appReducers/verifyReducer";
+// import searchBar from "./appReducers/searchBar";
+
+export default combineReducers({
+  // verifyReducer: verifyReducer,
+  // searchBar: searchBar,
+});

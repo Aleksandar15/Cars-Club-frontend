3a92b13ece1f7bcaafb38ce0f1a548de1706de7e
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-03-16 22:58:56 +0100

    Code Refactoring Redux Toolkit

--- src/redux/reduxCustomTypes/reduxActionTypes/reduxActionTypes.tsx
+++
@@ -1,5 +0,0 @@
-import { ThunkAction } from "@reduxjs/toolkit";
-import { AnyAction } from "redux";
-import { RootState } from "../../reducer/rootReducer";
-
-export type reduxActionType = ThunkAction<void, RootState, unknown, AnyAction>;

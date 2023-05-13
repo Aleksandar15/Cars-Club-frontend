6fbd2a215f1017ad36dc95a965d08bc252bcaa02
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-14 01:36:38 +0200

    Removed Bottleneck of useVerifyRefreshTK Hook and now it only runs based on condition received as argument

--- src/hooks/authHooks/useVerifyRefreshTK.ts
+++ src/hooks/authHooks/useVerifyRefreshTK.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import {
   useDispatchTyped,
   useSelectorTyped,
@@ -8,124 +7,104 @@ import {
   openModalAction,
   selectorOpenModalText,
 } from "../../redux/slices/openModalTextSlice";
-import {
-  authorize,
-  selectVerifyUser,
-  unauthorized,
-} from "../../redux/slices/verifySlice";
+import { authorize, unauthorized } from "../../redux/slices/verifySlice";
 import { axiosCredentials } from "../../utilities/API/axios";
 import {
   ErrorUserAuth,
   SuccessUserAuth,
 } from "../../utilities/Types/userAuthTypes";
 
-const useVerifyRefreshTK = (routeBelongsTo?: "private" | "public") => {
-  // const [authUser, setAuthUser] = useState<boolean | undefined>(undefined);
+const useVerifyRefreshTK = (
+  routeBelongsTo: "private" | "public" | "dynamic",
+  isUserAuthorized: boolean | undefined
+) => {
   const { isModalOpen } = useSelectorTyped(selectorOpenModalText);
   const dispatchTyped = useDispatchTyped();
 
-  // // Must use this isUserAuthorized Redux state for dependency. In case
-  // // when user clicks logout I want HomePage to refresh its buttons
-  const { isUserAuthorized } = useSelectorTyped(selectVerifyUser);
-  // console.log("isUserAuthorized useVerifyRefreshTK HOOK:", isUserAuthorized);
+  if (isUserAuthorized === undefined) {
+    const verifyRefreshToken = async () => {
+      try {
+        // GET /verifyrefreshtoken
+        const { data } = await axiosCredentials.get(
+          `/api/v1/auth/verifyrefreshtoken`
+        );
 
-  console.count("useVerifyRefreshTK CALLED");
-
-  useEffect(() => {
-    if (isUserAuthorized === undefined) {
-      const verifyRefreshToken = async () => {
-        try {
-          // GET /verifyrefreshtoken
-          const { data } = await axiosCredentials.get(
-            `/api/v1/auth/verifyrefreshtoken`
+        const dataTyped = data as SuccessUserAuth;
+        if (dataTyped?.isSuccessful) {
+          // Also on success update the isUserAuthorized state for NavBar
+          dispatchTyped(
+            authorize({
+              userStatus: {
+                isUserAuthorized: true,
+              },
+            })
           );
-
-          const dataTyped = data as SuccessUserAuth;
-          if (dataTyped?.isSuccessful) {
-            // setAuthUser(true);
-
-            // Also on success update the isUserAuthorized state for NavBar
+        }
+        // Error is handled in the Catch block
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          const errDataTyped = err?.response?.data as ErrorUserAuth;
+          if (errDataTyped?.isSuccessful === false) {
             dispatchTyped(
-              authorize({
+              unauthorized({
                 userStatus: {
-                  isUserAuthorized: true,
+                  isUserAuthorized: false,
+                  codeStatus: err?.response?.status,
                 },
               })
             );
-          }
-          // Error is handled in the Catch block
-        } catch (err) {
-          if (axios.isAxiosError(err)) {
-            const errDataTyped = err?.response?.data as ErrorUserAuth;
-            if (errDataTyped?.isSuccessful === false) {
-              // setAuthUser(false);
+
+            if (routeBelongsTo === "private") {
               dispatchTyped(
-                unauthorized({
-                  userStatus: {
-                    isUserAuthorized: false,
-                    codeStatus: err?.response?.status,
-                  },
+                openModalAction({
+                  isModalOpen: !isModalOpen,
+                  text: `Login please!`,
                 })
               );
+              // UPDATE2: This is half-buggy: tested in 100s tests &
+              // results: condition creates a little bit of delay -> a
+              // delay (luckily in this case) in a positive form: so it
+              // allows mid-React-calculations to be triggering
+              // ModalText's rendering inside the Login.tsx Component;
+              // otherwise to make this fail IS: to remove the
+              // condition OR to make it LESS complex like: `if (true)`
+              // or EVEN: `if (routeBelongsTo === "private" || true)`
+              // which always evaluates to `true` very quickly,
+              // but this below is still complex:
+              // `if (routeBelongsTo === "private" && true)` because
+              // React needs to calculate both the non-complex `true`
+              // && the complex conditions & thus allowing Login
+              // process to start & mid-execution runs openModalAction.
+              // ALSO: removing IF condition and adding setTimeout tests:
+              // not even setTimeout of 0ms or 10ms isn't
+              // triggering the ModalText to open, but it needs 100ms!
+              // I'm half-sure that this condition:
+              // `if (routeBelongsTo === "private")` is that COMPLEX
+              // with evaluation time around 100ms!?!
+              // However I'll keep the IF condition for security
+              // reasons mentioned bellow.
+              // Otherwise even if it doesn't (sometimes) run the ModalText
+              // was an optional UI/UX feature anyways!:)
 
-              if (routeBelongsTo === "private") {
-                dispatchTyped(
-                  openModalAction({
-                    isModalOpen: !isModalOpen,
-                    text: `Login please!`,
-                  })
-                );
-                // UPDATE2: This is half-buggy: tested in 100s tests &
-                // results: condition creates a little bit of delay -> a
-                // delay (luckily in this case) in a positive form: so it
-                // allows mid-React-calculations to be triggering
-                // ModalText's rendering inside the Login.tsx Component;
-                // otherwise to make this fail IS: to remove the
-                // condition OR to make it LESS complex like: `if (true)`
-                // or EVEN: `if (routeBelongsTo === "private" || true)`
-                // which always evaluates to `true` very quickly,
-                // but this below is still complex:
-                // `if (routeBelongsTo === "private" && true)` because
-                // React needs to calculate both the non-complex `true`
-                // && the complex conditions & thus allowing Login
-                // process to start & mid-execution runs openModalAction.
-                // ALSO: removing IF condition and adding setTimeout tests:
-                // not even setTimeout of 0ms or 10ms isn't
-                // triggering the ModalText to open, but it needs 100ms!
-                // I'm half-sure that this condition:
-                // `if (routeBelongsTo === "private")` is that COMPLEX
-                // with evaluation time around 100ms!?!
-                // However I'll keep the IF condition for security
-                // reasons mentioned bellow.
-                // Otherwise even if it doesn't (sometimes) run the ModalText
-                // was an optional UI/UX feature anyways!:)
+              // UPDATE: old notes below stopped working when Logic became
+              // more Complex I had to implement 'routeBelongsTo' argument.
+              // Bugs were: it (still) works well for ProtectedRoutes,
+              // but triggers ModalText unnecessarily for PublicRoutes.
 
-                // UPDATE: old notes below stopped working when Logic became
-                // more Complex I had to implement 'routeBelongsTo' argument.
-                // Bugs were: it (still) works well for ProtectedRoutes,
-                // but triggers ModalText unnecessarily for PublicRoutes.
-
-                // IMPORTANT NOTE ~ the reason why <ModalText/> is showing
-                // up (ONLY in ProtectedRoutes) is because it is already
-                // rendered inside Login.tsx
-                // (where I'm navigating at in case: userAuth === false),
-                // so I don't need to render it twice inside ProtectedRoutes.
-                // NOTE AS WELL ~ for non-protected AKA Public Routes like
-                // HomePage.tsx will not trigger the Modal, it's all safe:)
-              }
+              // IMPORTANT NOTE ~ the reason why <ModalText/> is showing
+              // up (ONLY in ProtectedRoutes) is because it is already
+              // rendered inside Login.tsx
+              // (where I'm navigating at in case: userAuth === false),
+              // so I don't need to render it twice inside ProtectedRoutes.
+              // NOTE AS WELL ~ for non-protected AKA Public Routes like
+              // HomePage.tsx will not trigger the Modal, it's all safe:)
             }
           }
         }
-      };
+      }
+    };
 
-      // if (isUserAuthorized === undefined) {
-      verifyRefreshToken();
-      // }
-    }
-  }, []);
-
-  // // // return authUser;
-  // // return isUserAuthorized;
-  // return authUser;
+    verifyRefreshToken();
+  }
 };
 export default useVerifyRefreshTK;

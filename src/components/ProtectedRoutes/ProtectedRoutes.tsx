1ed6a7d6f3a96bc4e559b794a073aada24eec1d0
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-13 02:38:28 +0200

    Code cleanup of ProtectedRoutes

--- src/components/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.tsx
@@ -1,21 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import useVerifyRefreshTK from "../../hooks/authHooks/useVerifyRefreshTK";
-import {
-  useDispatchTyped,
-  useSelectorTyped,
-} from "../../redux/reduxCustomTypes/ReduxTypedHooks/typedHooks";
-import {
-  openModalAction,
-  selectorOpenModalText,
-} from "../../redux/slices/openModalTextSlice";
-import { authorize } from "../../redux/slices/verifySlice";
-import { axiosCredentials } from "../../utilities/API/axios";
-import {
-  ErrorUserAuth,
-  SuccessUserAuth,
-} from "../../utilities/Types/userAuthTypes";
 import Loading from "../Loading/Loading";
 
 const ProtectedRoutes = () => {

99daf80d560e48fe7c35cd7721c72c59fbf13f67
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-15 22:08:19 +0200

    Updated ModalPost to have a form -> next is to sync it

--- src/components/Modals/ModalPost.tsx
+++ src/components/Modals/ModalPost.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
 import {
   useDispatchTyped,
   useSelectorTyped,
@@ -46,6 +45,65 @@ const ModalPost = () => {
     );
   };
 
+  // BODY STATES
+  const [postState, setPostState] = useState({
+    title: "",
+    image: "",
+    description: "",
+    contactNumber: "",
+    askingPrice: "",
+  });
+  const handlePostChanges = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name, files } = e.target;
+    console.log("value:", value);
+    // console.log("event.target.files:", e.target.files);
+    // const condition = files !== null || files !== undefined;
+    // console.log("condition", condition);
+    if (e.target?.files !== null) {
+      console.log("event.target.files[0]:", e.target.files[0]);
+    }
+
+    // If it's not a file-upload then use the 'value'
+    const valueCheck =
+      // This 'undefined' check was added for TextArea but
+      // TextArea's fix of <HTMLInputElement | HTMLTextAreaElement>
+      // Turns into issues with files & to avoid lots of IF statements
+      // I separated the TextArea changes in a new handler FN below.
+      e.target?.files !== null || e.target?.files !== undefined
+        ? value
+        : e.target?.files[0];
+
+    console.log("valueCheck:", valueCheck);
+
+    setPostState({ ...postState, [name]: valueCheck });
+  };
+
+  console.log("postState:", postState);
+
+  const handleTextAreaChanges = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setPostState({ ...postState, [name]: value });
+  };
+
+  const handlePostNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    const sanitizedValue = value.replace(/[^0-9]/g, ""); // Remove non-numeric characters
+
+    // This allows for state updates but it's no problem since ModalPost is unexpensive
+    setPostState({ ...postState, [name]: sanitizedValue });
+
+    // // Check if the value is a number or an empty string
+    // if (!isNaN(Number(value)) || value === "") {
+    //   // With this condition I can use [name]: value -> works as fine.
+    //   setPostState({ ...postState, [name]: sanitizedValue });
+    // }
+
+    // // Alternatively (same logic):
+    // if (!isNaN(Number(value)) || value === "") {
+    //   setPostState({ ...postState, [name]: value });
+    // }
+  };
+
   return (
     <>
       {isModalPostOpen && (
@@ -80,23 +138,94 @@ const ModalPost = () => {
             className="mymodal-wrapper"
           >
             {/* MODAL BODY */}
-            {/* <h3 className="h3ModalProjectTitle">
-              Title: <span className="spanModalProjectTitle"></span>
-            </h3>
-            <p className="pTagModalTechnologies">Technologies: </p>
-            <p className="pTagModalDescription">Description: </p> */}
 
-            {/* Text-only body for this case: */}
-            <h3
-              className="text-light pb-5 mb-5 mt-4"
-              style={{
-                // textDecoration: "underline overline",
-                textAlign: "center",
-                whiteSpace: "pre-line", // preferred over pre-wrap
-              }}
+            {/* TITLE */}
+            <div
+              // color wrapper
+              style={{ color: "white", textTransform: "uppercase" }}
+              className="pb-5 mb-5 mt-4 "
             >
-              {text}
-            </h3>
+              <form>
+                <label className="me-1 fw-bold" htmlFor="title">
+                  Title:
+                </label>
+                <input
+                  id="title"
+                  name="title"
+                  placeholder="Car name"
+                  value={postState.title}
+                  onChange={handlePostChanges}
+                />
+                {/* <br /> */}
+                <div className="mt-3">
+                  <label className="me-1 fw-bold" htmlFor="image">
+                    Image:
+                  </label>
+                  <input
+                    id="image"
+                    name="image"
+                    // value={postState.image}
+                    onChange={handlePostChanges}
+                    type="file"
+                    accept="image/jpeg, image/jpg"
+                  />
+                </div>
+                <label className="me-1 fw-bold mt-3" htmlFor="description">
+                  Description:
+                </label>
+                <br />
+                <textarea
+                  className="mt-3"
+                  id="description"
+                  name="description"
+                  placeholder="Description here ..."
+                  value={postState.description}
+                  onChange={handleTextAreaChanges}
+                  rows={3}
+                  cols={33}
+                />
+                <div>
+                  <label className="me-1 fw-bold mt-3" htmlFor="contact">
+                    Contact number:
+                  </label>
+                  {/* <br /> */}
+                  <input
+                    className="mt-3"
+                    id="contact"
+                    name="contactNumber"
+                    placeholder="888-888 (fake it)"
+                    value={postState.contactNumber}
+                    // onChange={handlePostChanges}
+                    onChange={handlePostNumberChange}
+                    type="tel"
+                  />
+                </div>
+                <div>
+                  <label className="me-2 fw-bold mt-3" htmlFor="price">
+                    Asking price:
+                  </label>
+                  <span style={{ fontWeight: "bold" }}>
+                    $
+                    <input
+                      className="mt-3"
+                      id="price"
+                      name="askingPrice"
+                      placeholder="8888 (fake it)"
+                      value={postState.askingPrice}
+                      // onChange={handlePostChanges}
+                      onChange={handlePostNumberChange}
+                      type="text"
+                      style={
+                        {
+                          // backgroundColor: "rgba(0, 0, 0, 0.5)",
+                          // opacity: "0.77",
+                        }
+                      }
+                    />
+                  </span>
+                </div>
+              </form>
+            </div>
 
             <div
               style={{
@@ -111,7 +240,8 @@ const ModalPost = () => {
                  text-info mb-2 fw-bold`}
                 type="button"
                 onClick={setShowModalFN}
-                className={"clickOKbutton"}
+                // className={"clickOKbutton"}
+                className={"clickOKbutton mt-5"}
               >
                 OKAY
               </Button>

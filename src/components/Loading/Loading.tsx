545d0ff0cca7f0dc079f818e87df4a67c17ea8a2
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-19 03:29:15 +0200

    Improved logic of Loading: ONLY HomePage.tsx will show Render's slow Loading emptathetic message

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,46 +1,17 @@
 function Loading() {
   return (
-    <>
-      <h1
-        style={{
-          textAlign: "center",
-          color: "blue",
-          borderRadius: "22px",
-          marginTop: "20px",
-          fontFamily: "Fantasy, Arial, Helvetica, sans-serif",
-          fontSize: "44px",
-        }}
-      >
-        LOADING . . .
-      </h1>
-      <h6
-        style={{
-          textAlign: "center",
-          color: "#0DCAF0",
-          // fontFamily: "Fantasy, Arial, Helvetica, sans-serif",
-          fontFamily: "Arial, Helvetica, sans-serif",
-          fontWeight: "bold",
-          marginTop: "20px",
-          // textTransform: "uppercase",
-          fontSize: "18px",
-        }}
-      >
-        Kindly await my Server deployed on Render to 'wake up' 😊
-      </h6>
-      <h6
-        style={{
-          textAlign: "center",
-          color: "#1BBF00",
-          fontFamily: "Arial, Helvetica, sans-serif",
-          marginTop: "22px",
-          fontSize: "15px",
-          // fontWeight: "bold",
-        }}
-      >
-        (Developer's notice: do not refresh; status 500 will re-call the server
-        after 30s)
-      </h6>
-    </>
+    <h1
+      style={{
+        textAlign: "center",
+        color: "blue",
+        borderRadius: "22px",
+        marginTop: "20px",
+        fontFamily: "Fantasy, Arial, Helvetica, sans-serif",
+        fontSize: "44px",
+      }}
+    >
+      LOADING . . .
+    </h1>
   );
 }
 

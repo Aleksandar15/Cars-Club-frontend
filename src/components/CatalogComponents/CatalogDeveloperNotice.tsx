cb26d480125674e0d9a878b1883875959a84ba1c
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-20 23:04:04 +0200

    Updated Developer's Notice about my pagination 1 post/page

--- src/components/CatalogComponents/CatalogDeveloperNotice.tsx
+++ src/components/CatalogComponents/CatalogDeveloperNotice.tsx
@@ -4,18 +4,28 @@ import useMyNavigate from "../../hooks/useMyNavigate/useMyNavigate";
 const CatalogDeveloperNotice = () => {
   const navigatePage = useMyNavigate();
   return (
-    <h5
-      // className="mb-3 pb-3 text-danger text-center"
-      className="mb5 pb-5 text-danger text-center"
-    >
-      Developer's notice: the CRUD operations happen at{" "}
-      <Button
-        className="p-1" // made smaller
-        onClick={() => navigatePage("/marketplace")}
+    <>
+      <h5
+        // className="mb-3 pb-3 text-danger text-center"
+        // className="mb-5 pb-5 text-danger text-center"
+        // className="mb-5  text-danger text-center"
+        className="  text-danger text-center"
       >
-        Marketplace
-      </Button>
-    </h5>
+        Developer's notice: the CRUD operations happen at{" "}
+        <Button
+          className="p-1" // made smaller
+          onClick={() => navigatePage("/marketplace")}
+        >
+          Marketplace
+        </Button>
+      </h5>
+      <h6
+        className="mb-5    mt-3 text-center"
+        style={{ color: "#0dcaf0", fontSize: "18px" }}
+      >
+        (In here I have 1 post/page so you can easily access pagination.)
+      </h6>
+    </>
   );
 };
 

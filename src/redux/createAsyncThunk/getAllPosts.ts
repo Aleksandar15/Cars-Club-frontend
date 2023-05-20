dc7810e84e8734df410e1299a4392e23a824ba46
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-21 00:45:37 +0200

    Added very important notes about my axiosCredentials interceptor instance REQUIRED to be used in AsyncThunk but requires a workarounds since I can't call my Custom Hooks useAxiosInstance inside of my getAllPosts Async Thunk!

--- src/redux/createAsyncThunk/getAllPosts.ts
+++ src/redux/createAsyncThunk/getAllPosts.ts
@@ -6,12 +6,37 @@ import {
   PayloadAction,
 } from "@reduxjs/toolkit";
 import useAxiosInterceptor from "../../hooks/authHooks/useAxiosInterceptor";
-import { axiosCredentials } from "../../utilities/API/axios";
+import {
+  axiosCredentials,
+  axiosCredentialsNonInterceptors, // Expectedly fails->read the rest below.
+} from "../../utilities/API/axios";
 import { PostType } from "../../utilities/Types/postsTypes";
 import { RootState } from "../store";
 
 // const axiosCredentials = useAxiosInterceptor();
 // // ^ Invalid hook call. Only inside of the body of a fn component.
+//
+// NEW IMPORTANT INFO ABOUT HOW axiosCredentials still is an interceptor:
+// UPDATE2: the success from importing axisoCredentials from axios Utils
+// is because the same reference is grabbed which equals to the same
+// instance that is being intercepted inside my useAxiosInterceptor custom
+// Hook being called inside ModalPost.tsx -> which in here I'm using
+// the same REFERENCE of the same INTERCEPTED AXIOS INSTANCE.
+//
+// In case where I wouldn't need to call this function in ModalPost
+// a workaround would be to call it just for the sake of gettig in here
+// the intercepted Axios instance object & avoiding the invalid Hook call.
+// #1st workaround is confirmed by the fact that when I've COMMENTED OUT
+// the useAxiosInterceptor custom Hook CALL inside my ModalPost.tsx
+// & this getAllPosts() being called in an useEffect in ModalPost is
+// failing with 401 (not my custom invalid accessToken message though).
+// (and bellow CATCH doesn't throw any errors so instead I get
+// .length errors and a Blank page is fixed by my `return []` update.)
+// #2nd workaround would be perhaps to create another Util that calls
+// the Hook like "import useAxiosInterceptor" ->
+// "const axiosInterceptor = useAxiosInterceptor()"
+// -> "export default axiosInterceptor" -> and in here (AsyncThunk)
+// I'll IMPORT that instance from that custom Util.
 
 // Define the async thunk
 export const getAllPosts: AsyncThunk<PostType[], void, {}> = createAsyncThunk(
@@ -20,6 +45,9 @@ export const getAllPosts: AsyncThunk<PostType[], void, {}> = createAsyncThunk(
     // thunkAPI must be 2nd argument
     try {
       const { data } = await axiosCredentials("/api/v1/post/getallposts");
+      // const { data } = await axiosCredentialsNonInterceptors( // Expectedly fails
+      //   "/api/v1/post/getallposts"
+      // );
       // return data; // Works but I rather access the real data:
       // Because destructuring this in Post component causes TSC error
 
@@ -30,7 +58,13 @@ export const getAllPosts: AsyncThunk<PostType[], void, {}> = createAsyncThunk(
       // throw new Error("Error fetching posts");
       // Instead answer by Drew Reese stackoverflow:
       // https://stackoverflow.com/questions/76077610/react-redux-data-not-being-passed-correctly-components-with-dynamic-routes/76078197#76078197
-      thunkAPI.rejectWithValue(error);
+      thunkAPI.rejectWithValue(error); // does nothing good
+      // ^ UPDATE: Does nothing when request fails with 401 (invalid accessToken)
+      // when using for tests: axiosCredentialsNonInterceptors OR commenting out
+      // the useAxiosInterceptor call inside ModalPost (where I use this Async Thunk)
+      // so instead I manually fix the error:
+      // "Uncaught TypeError: Cannot read properties of undefined (reading 'length')"
+      return [];
     }
   }
 );
@@ -71,6 +105,7 @@ const postsSlice = createSlice({
         state.posts = action.payload;
       })
       .addCase(getAllPosts.rejected, (state, action) => {
+        console.log("rejected STATE:", state, "rejected aciton:", action);
         state.status = "failed";
         state.error =
           action.error.message === typeof "string"

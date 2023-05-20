2077efbc42238a6a0440cb9e8f36f0abff96dc2c
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-20 03:31:38 +0200

    Brought back the old way of verifying PublicRoutes & redirecting as per needed as it's working smoothly once I've removed my Tests about User info and Redux state

--- src/components/PublicRoutes/PublicRoutes.tsx
+++ src/components/PublicRoutes/PublicRoutes.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import useVerifyRefreshTK from "../../hooks/authHooks/useVerifyRefreshTK";
-import useVerifyRefreshTK_FN from "../../hooks/authHooks/useVerifyRefreshTK_FN";
 import { useSelectorTyped } from "../../redux/reduxCustomTypes/ReduxTypedHooks/typedHooks";
 import { selectVerifyUser } from "../../redux/slices/verifySlice";
 import Loading from "../Loading/Loading";
@@ -9,17 +8,7 @@ import Loading from "../Loading/Loading";
 const PublicRoutes = () => {
   const { isUserAuthorized } = useSelectorTyped(selectVerifyUser);
 
-  // useVerifyRefreshTK("public", isUserAuthorized);
-  const verifyRefreshTokenFN = useVerifyRefreshTK_FN();
-
-  // By having it in Effect it only runs on HARD REFRESH:)
-  // fixed: double-running useVerifyRefreshTK by Login.tsx
-  useEffect(() => {
-    console.count("PublicRoutes EFFECT RUNS");
-    if (isUserAuthorized === undefined) {
-      verifyRefreshTokenFN();
-    }
-  }, []);
+  useVerifyRefreshTK("public", isUserAuthorized);
 
   const location = useLocation();
 

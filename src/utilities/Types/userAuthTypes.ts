7334c95f7634f65692b07a5ec64fc2abc5ceadb2
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-20 03:16:01 +0200

    Fixed Types for SuccessUserAuth who uses useVerifyRefreshTK Custom Hook

--- src/utilities/Types/userAuthTypes.ts
+++ src/utilities/Types/userAuthTypes.ts
@@ -4,6 +4,9 @@ export interface ErrorUserAuth {
 }
 export interface SuccessUserAuth extends ErrorUserAuth {
   user_role: string;
+  user_name?: string;
+  user_email?: string;
+  user_id?: string;
 }
 
 export interface LogoutSuccess {

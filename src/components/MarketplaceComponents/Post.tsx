e6c2e0ae9ad1e4cfc693e12e3c008cc8b73843e0
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-21 08:12:43 +0200

    Separated Edit/Delete buttons from Post parent component -> moved them as children who receives 2 props of IDS

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
+// import useAxiosInterceptor from "../../hooks/authHooks/useAxiosInterceptor";
 import useModalPost_formatNum from "../../hooks/ModalPostHooks/useModalPost_formatNum";
 import {
   getAllPosts,
@@ -14,33 +16,18 @@ import {
 import { selectVerifyUser } from "../../redux/slices/verifySlice";
 import { Currency, PostType } from "../../utilities/Types/postsTypes";
 import Loading from "../Loading/Loading";
+import Post_Action_Buttons from "./Post_Action_Buttons";
 
 function Post() {
+  // const axiosCredentials = useAxiosInterceptor();
+
   // const handleDelete = () => {
   //   onDelete();
   // };
 
-  // const handleEdit = () => {
-  //   onEdit();
+  // const handleEdit = async (post_id: string, user_id: string) => {
+  //   console.log("Edit clicked");
   // };
-
-  //
-  // const [posts, setPosts] = useState<PostType[]>([]);
-  // // Posts empty data so that I can check copy & reset state if needed
-  // const postsInitial = [
-  //   {
-  //     post_title: "",
-  //     post_image_buffer: undefined,
-  //     post_description: "",
-  //     post_contact_number: "",
-  //     post_asking_price: "",
-  //     post_asking_price_currency: "",
-  //     post_id: "",
-  //     user_id: "",
-  //     post_created_at: "",
-  //   },
-  // ];
-  // console.log('posts:",posts:', posts);
 
   const convertBufferToImgSRC = (bufferData: ArrayBufferLike | undefined) => {
     const buffer = new Uint8Array(bufferData as ArrayBufferLike).buffer;
@@ -92,7 +79,7 @@ function Post() {
                   post.post_created_by_user_role === "owner" ||
                   post.post_created_by_user_role === "super-admin" ? (
                     <ButtonGroup>
-                      <Button
+                      {/* <Button
                         className="bg-warning btn-outline-warning text-light"
                         style={{
                           maxWidth: "70px",
@@ -100,7 +87,7 @@ function Post() {
                           zIndex: "0",
                           // marginLeft: "0px",
                         }}
-                        onClick={() => console.log("Edit clicked")}
+                        onClick={() => handleEdit(post.post_id, post.user_id)}
                       >
                         Edit
                       </Button>
@@ -109,7 +96,12 @@ function Post() {
                         style={{ maxWidth: "70px", width: "70px" }}
                       >
                         Delete
-                      </Button>
+                      </Button> */}
+                      {/* ^^^^^UPDATED INTO: */}
+                      <Post_Action_Buttons
+                        post_post_id={post.post_id as string}
+                        post_user_id={post.user_id as string}
+                      />
                     </ButtonGroup>
                   ) : null}
                 </div>
@@ -161,114 +153,6 @@ function Post() {
             </div>
           );
         })}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      <div className="post">
-        <div className="post-header">
-          {/* <h2>{title}</h2> */}
-          <h2>{"title"}</h2>
-          <div className="post-actions">
-            {/* <button onClick={handleDelete}>Delete</button>
-          <button onClick={handleEdit}>Edit</button> */}
-            <ButtonGroup>
-              <Button
-                className="bg-warning btn-outline-warning text-light"
-                // variant="bg-warning btn-outline-warning text-light"
-                style={{
-                  // transform: "scale(1)", // Enlarges the button if value is '2'+
-                  // transform: "scale(-1)", // Flips the button upside down
-                  maxWidth: "70px",
-                  width: "70px",
-
-                  // Styling issue was: react-bootstrap wise
-                  // left-button's border is hovering over its right-sibling, then
-                  // in DevTools I found out both of these styling below were causing
-                  // that issue and fix is: either zIndex to 0; on the left button or
-                  // marginLeft on the right (Delete) button would fix the issue.
-                  zIndex: "0",
-                  // marginLeft: "0px", // Can be used on the right ('delete') button
-                }}
-                onClick={() => console.log("Edit clicked")}
-              >
-                Edit
-              </Button>
-              <Button
-                className="bg-danger btn-outline-danger text-light"
-                style={{ maxWidth: "70px", width: "70px" }}
-              >
-                Delete
-              </Button>
-            </ButtonGroup>
-          </div>
-        </div>
-        <div className="post-image-wrapper-center">
-          <div className="post-image-wrapper">
-            {/* <img src={image} alt={title} /> */}
-            <img
-              src={
-                "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/16q1/665058/2017-mercedes-benz-e-class-photos-and-info-news-car-and-driver-photo-665186-s-original.jpg"
-              }
-              alt={"title"}
-              className="post-image"
-            />
-          </div>
-        </div>
-        {/* <p>{description}</p> */}
-        <p>{"description ".repeat(100)}</p>
-        <div className="post-comments">
-          <h6>Comments:</h6>
-          {/* <ul className="post-comments-ul">
-          {comments.map((comment, index) => (
-            <li key={index} className="post-comments-li">{comment}</li>
-          ))}
-        </ul> */}
-        </div>
-      </div>
-      <div className="post">
-        <div className="post-header">
-          <h2>{"title"}</h2>
-          <div className="post-actions">
-            <ButtonGroup>
-              <Button
-                className="bg-warning btn-outline-warning text-light"
-                style={{
-                  maxWidth: "70px",
-                  width: "70px",
-                  zIndex: "0",
-                  // marginLeft: "0px",
-                }}
-                onClick={() => console.log("Edit clicked")}
-              >
-                Edit
-              </Button>
-              <Button
-                className="bg-danger btn-outline-danger text-light"
-                style={{ maxWidth: "70px", width: "70px" }}
-              >
-                Delete
-              </Button>
-            </ButtonGroup>
-          </div>
-        </div>
-        <div className="post-image-wrapper-center">
-          <div className="post-image-wrapper">
-            <img
-              src={
-                "https://images.unsplash.com/photo-1602596322568-33cca0e8b4ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVyY2VkZXMlMjBlJTIwY2xhc3N8ZW58MHx8MHx8"
-              }
-              alt={"title"}
-              className="post-image"
-            />
-          </div>
-        </div>
-        <p>{"description ".repeat(100)}</p>
-        <div className="post-comments">
-          <h6>Comments:</h6>
-        </div>
-      </div>
     </>
   );
 }

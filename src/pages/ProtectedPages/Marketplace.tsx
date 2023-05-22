c817f81c03f805210a0375f078c689a50d08388b
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-22 06:25:29 +0200

    Rendering ModalDeletePost

--- src/pages/ProtectedPages/Marketplace.tsx
+++ src/pages/ProtectedPages/Marketplace.tsx
@@ -1,13 +1,15 @@
 import FormSearchCars from "../../components/MarketplaceComponents/FormSearchCars";
 import MarketplaceHeader from "../../components/MarketplaceComponents/MarketplaceHeader";
 import Post from "../../components/MarketplaceComponents/Post";
+import ModalDeletePost from "../../components/Modals/ModalDeletePost";
 import ModalPost from "../../components/Modals/ModalPost";
 import ModalPostSuccessText from "../../components/Modals/ModalPostSuccessText";
-import ModalText from "../../components/Modals/ModalText";
+// import ModalText from "../../components/Modals/ModalText";
 
 const Marketplace = () => {
   return (
     <>
+      <ModalDeletePost />
       <ModalPostSuccessText />
       <ModalPost />
       <MarketplaceHeader />

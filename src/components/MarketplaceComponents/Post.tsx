324c49eabda4037ff6762e0bec48fe707e74eaa3
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2024-06-21 23:59:36 +0200

    Improving styling in comment sections

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -315,7 +315,8 @@ function Post() {
                       // marginLeft: "10px",
                       backgroundColor: "#f8f9fa",
                       // border: "1px solid #ddd",
-                      border: "1px dotted cyan",
+                      // border: "1px dotted cyan",
+                      border: "2px dotted cyan",
                       listStyleType: "disc",
                       cursor: "default",
                     }}

8189cc493c3aa8a8513f3451797c60e88e72e963
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2024-07-17 23:56:48 +0200

    Tests with Negative margin styling tests in the comment sections: might modify later

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -322,7 +322,8 @@ function Post() {
                       // margin: "-12px 18px",
                       // margin: "-8px 18px",
                       // margin: "-8px -22px",
-                      margin: "-8px -2px",
+                      // margin: "-8px -2px",
+                      margin: "-18px -2px",
                       //
                       // backgroundColor: "#f8f9fa",
                       // backgroundColor: "magenta",

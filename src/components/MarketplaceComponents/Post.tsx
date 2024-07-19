40c797903b64d3c070d1f8b3962f9d3f68554d37
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2024-07-19 23:59:05 +0200

    Using Negative margin numbers (#s) -> styling tests in the comment sections: might modify later

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -324,7 +324,8 @@ function Post() {
                       // margin: "-8px -22px",
                       // margin: "-8px -2px",
                       // margin: "-18px -2px",
-                      margin: "-8px -15px",
+                      // margin: "-8px -15px",
+                      margin: "-12px -15px",
                       //
                       // backgroundColor: "#f8f9fa",
                       // backgroundColor: "magenta",

0b0be3b935d3ab3b765f602806cb830849cf158f
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2024-08-10 23:59:38 +0200

    Added className to button inside comment sections

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -307,6 +307,7 @@ function Post() {
                   // style={color="red"}
                   // style={(color = "red")} //VScode wrong corrections
                   style={{ color: "red", cursor: "default" }}
+                  className="fw-bold"
                   // // ></Button>
                 ></button>
                 <ul

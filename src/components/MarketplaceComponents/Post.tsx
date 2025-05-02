0525807fecd9d750d6df61ba6e52b4c66cf3f10f
Aleksandar15 <aleksandar15a@outlook.com>
2025-05-02 07:48:53 +0200

    Added h6 element notice for comment feature incoming

--- src/components/MarketplaceComponents/Post.tsx
+++ src/components/MarketplaceComponents/Post.tsx
@@ -330,97 +330,101 @@ function Post() {
                   </Button>
                 </p>
                 {isCommentSectionVisible && (
-                  <div>
-                    <ul
-                      // className="list-group"
-                      className="post-comments-ul"
-                    >
-                      {/* <li
-                        // // {/* {comments.map((comment, index) => (
-                        // // {/* @
-                        // // {/* Symbol `@` for UI can't be in here as it's started on a new line
-                        // // because it's considered a new item.
-                        // key={index}
-                        className="list-group-item"
-                        // Note: this className removes 'dot' sign prior to the listed items => NEEDS FIX
-                        style={{
-                          // padding: "10px",
-                          padding: "3px",
-                          // marginLeft: "10px",
-                          // margin: "10px", // testing phase
-                          // margin: "10px 5px", // testing phase
-                          // margin: "10px 8px", // testing phase
-                          // margin: "10px 18px", //
-                          // margin: "12px 18px",
-                          // margin: "-20px 18px",
-                          // margin: "-12px 18px",
-                          // margin: "-8px 18px",
-                          // margin: "-8px -22px",
-                          // margin: "-8px -2px",
-                          // margin: "-18px -2px",
-                          // margin: "-8px -15px",
-                          margin: "-12px -15px",
-                          //
-                          // backgroundColor: "#f8f9fa",
-                          // backgroundColor: "magenta",
-                          // backgroundColor: "smokewhite",
-                          // backgroundColor: "darkblue",
-                          // border: "1px solid #ddd",
-                          // border: "1px dotted cyan",
-                          // border: "2px dotted cyan",
-                          // border: "3px dotted cyan",
-                          // border: "3px dotted red", //modify it to fit the rest of the page
-                          border: "3px dotted darkblue",
-                          // borderRadius: "10px",
-                          // borderRadius: "10px 8px",
-                          // borderRadius: "10px 6px",
-                          // borderRadius: "12.5px",
-                          borderRadius: "15.5px",
-                          listStyleType: "disc",
-                          cursor: "default",
-                          //
-                          // color: "red",
-                          color: "darkblue",
-                        }}
-                        //
-                        // value={post.post_created_by_user_name}
-                      >
-                        @{post.post_created_by_user_name}
-                      </li> */}
-                      <li
-                      // {/* ))}  // - These braces are for closing the above comment.map method which is commented out for now since there is no full functionality yet.
-                      // Test2 - this second test user should be removed as comment.map method above should be used in the future.
-                      // className="list-group-item"
-                      // // With & without this className there is a visual difference
-                      // // where "dot" is not added if there's no  className so I should make a UI decision.
-                      >
-                        @Test
-                      </li>
-                    </ul>
-                    <form
-                    // onSubmit={handleCommentSubmit}
-                    >
-                      <div className="form-group">
-                        <label htmlFor="comment">Add a comment:</label>
-                        <textarea
-                          id="comment"
-                          className="form-control"
-                          // rows="3" // Data type is supposed to be a number.
-                          rows={3}
-                          // value={newComment}
-                          // onChange={handleCommentChange}
-                          // >
-                          // // NOTE:
-                          // // DO NOT CLOSE IT LIKE BELOW, since this textarea tag above will be a self-closing tag.
-                          // // {/* Test text
-                          // // </textarea>
-                        />
-                      </div>
-                      <button type="submit" className="btn btn-primary">
-                        Submit
-                      </button>
-                    </form>
-                  </div>
+                  <h6>
+                    {/* Comment feature incoming. */}
+                    Comment feature coming soon.
+                  </h6>
+                  // <div>
+                  //   <ul
+                  //     // className="list-group"
+                  //     className="post-comments-ul"
+                  //   >
+                  //     {/* <li
+                  //       // // {/* {comments.map((comment, index) => (
+                  //       // // {/* @
+                  //       // // {/* Symbol `@` for UI can't be in here as it's started on a new line
+                  //       // // because it's considered a new item.
+                  //       // key={index}
+                  //       className="list-group-item"
+                  //       // Note: this className removes 'dot' sign prior to the listed items => NEEDS FIX
+                  //       style={{
+                  //         // padding: "10px",
+                  //         padding: "3px",
+                  //         // marginLeft: "10px",
+                  //         // margin: "10px", // testing phase
+                  //         // margin: "10px 5px", // testing phase
+                  //         // margin: "10px 8px", // testing phase
+                  //         // margin: "10px 18px", //
+                  //         // margin: "12px 18px",
+                  //         // margin: "-20px 18px",
+                  //         // margin: "-12px 18px",
+                  //         // margin: "-8px 18px",
+                  //         // margin: "-8px -22px",
+                  //         // margin: "-8px -2px",
+                  //         // margin: "-18px -2px",
+                  //         // margin: "-8px -15px",
+                  //         margin: "-12px -15px",
+                  //         //
+                  //         // backgroundColor: "#f8f9fa",
+                  //         // backgroundColor: "magenta",
+                  //         // backgroundColor: "smokewhite",
+                  //         // backgroundColor: "darkblue",
+                  //         // border: "1px solid #ddd",
+                  //         // border: "1px dotted cyan",
+                  //         // border: "2px dotted cyan",
+                  //         // border: "3px dotted cyan",
+                  //         // border: "3px dotted red", //modify it to fit the rest of the page
+                  //         border: "3px dotted darkblue",
+                  //         // borderRadius: "10px",
+                  //         // borderRadius: "10px 8px",
+                  //         // borderRadius: "10px 6px",
+                  //         // borderRadius: "12.5px",
+                  //         borderRadius: "15.5px",
+                  //         listStyleType: "disc",
+                  //         cursor: "default",
+                  //         //
+                  //         // color: "red",
+                  //         color: "darkblue",
+                  //       }}
+                  //       //
+                  //       // value={post.post_created_by_user_name}
+                  //     >
+                  //       @{post.post_created_by_user_name}
+                  //     </li> */}
+                  //     <li
+                  //     // {/* ))}  // - These braces are for closing the above comment.map method which is commented out for now since there is no full functionality yet.
+                  //     // Test2 - this second test user should be removed as comment.map method above should be used in the future.
+                  //     // className="list-group-item"
+                  //     // // With & without this className there is a visual difference
+                  //     // // where "dot" is not added if there's no  className so I should make a UI decision.
+                  //     >
+                  //       @Test
+                  //     </li>
+                  //   </ul>
+                  //   <form
+                  //   // onSubmit={handleCommentSubmit}
+                  //   >
+                  //     <div className="form-group">
+                  //       <label htmlFor="comment">Add a comment:</label>
+                  //       <textarea
+                  //         id="comment"
+                  //         className="form-control"
+                  //         // rows="3" // Data type is supposed to be a number.
+                  //         rows={3}
+                  //         // value={newComment}
+                  //         // onChange={handleCommentChange}
+                  //         // >
+                  //         // // NOTE:
+                  //         // // DO NOT CLOSE IT LIKE BELOW, since this textarea tag above will be a self-closing tag.
+                  //         // // {/* Test text
+                  //         // // </textarea>
+                  //       />
+                  //     </div>
+                  //     <button type="submit" className="btn btn-primary">
+                  //       Submit
+                  //     </button>
+                  //   </form>
+                  // </div>
                 )}
               </div>
               {/* V2 ends. */}
